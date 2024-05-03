aaf41a1e2eda788499a8491d577d1528f6f2b6d2
Nipaaaa1 <Frostone224@gmail.com>
2024-05-03 17:44:39 +0700

    feat: add admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full">
+    <LayoutNav>
+      <LayoutNavLinks href="/admin">Dashboard</LayoutNavLinks>
+      <LayoutNavLinks href="/admin/products">Products</LayoutNavLinks>
+      <LayoutNavLinks href="/admin/users">Customers</LayoutNavLinks>
+      <LayoutNavLinks href="/admin/orders">Sales</LayoutNavLinks>
+    </LayoutNav>
+    <main class="container my-6 mx-auto">
+      <slot />
+    </main>
+  </div>
+</template>
