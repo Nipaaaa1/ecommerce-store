fb160f2622248069d26feebd543dbf0a234d1dab
Nipaaaa1 <Frostone224@gmail.com>
2024-05-03 17:43:43 +0700

    feat: add Nav Links component

---
+++ components/layout/Nav/Links.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { cn } from "~/lib/utils";
+const route = useRoute();
+const routeName = "/" + route.name?.toString();
+
+defineProps<{
+  href: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink
+    :href="href"
+    :class="
+      cn(
+        'px-8 py-4 hover:bg-secondary hover:text-secondary-foreground',
+        routeName === href && 'bg-background text-foreground',
+      )
+    "
+    ><slot
+  /></NuxtLink>
+</template>
