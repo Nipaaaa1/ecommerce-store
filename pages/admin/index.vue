b1f94f75c594aa1f8973d5932349e95a13d042f6
Nipaaaa1 <Frostone224@gmail.com>
2024-05-07 15:23:07 +0700

    feat: add admin dashboard

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,10 +2,38 @@
 definePageMeta({
   layout: "admin",
 });
+
+const [salesData, userData, productData] = await Promise.all([
+  useFetch("/api/sales", {
+    pick: ["amount", "numberOfSales"],
+  }),
+  useFetch("/api/user", {
+    pick: ["userCount", "averageValuePerUser"],
+  }),
+  useFetch("/api/product", {
+    pick: ["activeCount", "inactiveCount"],
+  }),
+]);
 </script>
 
 <template>
   <NuxtLayout>
-    <h1>This is admin page</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <LayoutCard
+        title="Sales"
+        :subtitle="`${formatNumber(salesData.data.value?.numberOfSales as number)} orders`"
+        :body="formatCurrency(salesData.data.value?.amount as number)"
+      />
+      <LayoutCard
+        title="Customers"
+        :subtitle="`${formatCurrency(userData.data.value?.averageValuePerUser as number)} Average Value`"
+        :body="formatNumber(userData.data.value?.userCount as number)"
+      />
+      <LayoutCard
+        title="Active Product"
+        :subtitle="`${formatNumber(productData.data.value?.inactiveCount as number)} Inactive`"
+        :body="formatNumber(productData.data.value?.activeCount as number)"
+      />
+    </div>
   </NuxtLayout>
 </template>
