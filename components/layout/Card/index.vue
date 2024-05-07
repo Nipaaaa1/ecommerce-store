d4975ca2d692dd4c69987639533fc58041fb6865
Nipaaaa1 <Frostone224@gmail.com>
2024-05-07 14:17:27 +0700

    feat: create card component

---
+++ components/layout/Card/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  subtitle: string;
+  body: string;
+}>();
+</script>
+
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>{{ title }}</CardTitle>
+      <CardDescription>{{ subtitle }}</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p>
+        {{ body }}
+      </p>
+    </CardContent>
+  </Card>
+</template>
